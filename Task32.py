583c67c6a29dbe0e285b5b95a082cb9a83f17b02
Boguckaya_is <Boguckay@mail.ru>
2023-03-02 15:51:42 +0300

    Задача 32

--- Task32.py
+++ Task32.py
@@ -3,9 +3,19 @@
 заданного минимума и не больше заданного максимума)'''
 
 
-list_1 = [-5, 9, 0, 3, -1, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5,-5, 7]
+from Module_for_HW_6 import create_list, indexes_of_numbers
+
+num_list = create_list(int(input('Введите размер массива: ')))
+left_endpoint = int(input('Введите минимум диапазона: '))
+right_endpoint = int(input('Введите максимум диапазона: '))
+
+print(*num_list)
+print(*indexes_of_numbers(num_list, left_endpoint, right_endpoint))
+
+
+'''list_1 = [-5, 9, 0, 3, -1, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5,-5, 7]
 min_number = int(input())
 max_number = int(input())
 for i in range(len(list_1)):
     if min_number <= list_1[i] <= max_number:
-        print(i)
+        print(i)''
